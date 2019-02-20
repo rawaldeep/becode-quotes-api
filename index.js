9fa562c162d24c6ca52e8d14f02530cb745a40f4
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-02-20 22:10:41 +0100

    changes in package.json

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.engine('hbs', exphbs({
   layoutsDir: __dirname + '/views/layouts/'
 }));
 app.set('view engine', 'hbs');
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running`);
